aa3152559f7e00bc8ff0bac3bc03941c04719445
Hafidh <hafidh2704@gmail.com>
2022-12-06 09:50:04 +0700

    Test GET Todos

--- 6_API_Testing/tests/integration/todo.controller.int.test.js
+++ 6_API_Testing/tests/integration/todo.controller.int.test.js
@@ -1,4 +1,4 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, test } from "vitest";
 const request = require("supertest");
 const app = require("../../app");
 const newTodo = require("../mock-data/new-todo.json");
@@ -6,10 +6,26 @@ const newTodo = require("../mock-data/new-todo.json");
 const endpointUrl = "/todos/";
 
 describe(endpointUrl, () => {
+  test(`GET ${endpointUrl}`, async () => {
+    const response = await request(app).get(endpointUrl);
+    expect(response.statusCode).toBe(200);
+    expect(typeof response.body).toBeTruthy();
+    expect(response.body[0].title).toBeDefined();
+    expect(response.body[0].done).toBeDefined();
+  });
   it(`POST ${endpointUrl}`, async () => {
     const response = await request(app).post(endpointUrl).send(newTodo);
     expect(response.statusCode).toBe(201);
     expect(response.body.title).toBe(newTodo.title);
     expect(response.body.done).toBe(newTodo.done);
+  });
+  it(`should return error 500 on malformed data with POST ${endpointUrl}`, async () => {
+    const response = await await request(app)
+      .post(endpointUrl)
+      .send({ title: "Missing done property" });
+    expect(response.statusCode).toBe(500);
+    expect(response.body).toStrictEqual({
+      message: "Todo validation failed: done: Path `done` is required.",
+    });
   });
 });
